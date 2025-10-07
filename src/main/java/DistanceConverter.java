04d25882a87ebd645ebe804a23b7f600fe7d52e4
LucasHutchinson08 <03lucas.hutchinson@kamsc.org>
2025-10-06 21:49:38 -0400

    code

--- src/main/java/DistanceConverter.java
+++ src/main/java/DistanceConverter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DistanceConverter {
     public static void main(String[] args) {
         // TODO: Implement distance converter
@@ -7,5 +9,53 @@ public class DistanceConverter {
         // - Use conversion factor: 1 mile = 1.60935 kilometers
         // - Display conversion results
         // - Handle invalid menu choices
+        Scanner input = new Scanner(System.in);
+        int choice = 0;
+
+        while (true) {
+            System.out.println("Choose conversion direction");
+            System.out.println("1. Convert Miles to Kilometers");
+            System.out.println("2. Convert Kilometers to Miles");
+            System.out.println("3. Exit");
+            System.out.println();
+
+            System.out.println("Enter your choice (1, 2, or 3): ");
+            choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter distance in miles: ");
+                    double distanceM = input.nextDouble();
+                    System.out.println("Conversion Results:");
+                    System.out.println("Miles: " + distanceM);
+                    System.out.println("Kilometers: " + (distanceM * 1.60934));
+                    System.out.println();
+                    System.out.println();
+
+
+                    break;
+                case 2:
+                    System.out.println("Enter distance in kilometers: ");
+                    double distanceK = input.nextDouble();
+                    System.out.println("Conversion Results: ");
+                    System.out.println("Kilometers: " + distanceK);
+                    System.out.println("Miles: " + (distanceK / 1.60934));
+                    System.out.println();
+                    System.out.println();
+
+
+                    break;
+                case 3:
+                    System.out.print("Goodbye!");
+                    System.out.println();
+
+
+            }
+
+
+
+        }
+
+
     }
 }
